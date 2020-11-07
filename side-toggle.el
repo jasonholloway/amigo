6df5305b0d8ecf25b269c3547617505fa5305d15
Jason Holloway <miceeatnicerice@hotmail.com>
2020-11-07 18:47:34 +0000

    more progress

--- side-toggle.el
+++ side-toggle.el
@@ -14,23 +14,41 @@
 
 (side-toggle--find-window)
 
-(window-parameters
- (side-toggle--find-window))
-
 (window-total-width
  (side-toggle--find-window))
 
 
 (side-toggle--current-mode)
-(side-toggle-toggle)
 
 (side-toggle--find-buffer)
 (side-toggle--open-buffer)
 
+(side-toggle--find-window)
+(side-toggle--open-window (side-toggle--find-buffer))
 
+(window-parameters
+ (side-toggle--find-window))
+
+(defun side-toggle--open-window (buffer)
+  "Blah BUFFER."
+  (display-buffer-in-side-window
+    buffer
+    '((side . right)
+      (window-parameters . ((side-toggle--window-tag . t))))))
+
+(defun side-toggle--close-window (window)
+  "Blah WINDOW."
+  (let ((width (window-total-width window)))
+    (set-frame-parameter nil 'side-toggle-width 123)
+    (delete-window window)))
+
+(frame-parameters nil)
+
+(side-toggle-toggle)
 
 
 (defconst side-toggle--buffer-name-prefix " *side-toggle-")
+(defconst side-toggle--window-tag "side-toggle")
 
 ;;;###autoload
 (defun side-toggle-toggle ()
@@ -48,37 +66,26 @@
     ((side-toggle--find-buffer) 'exists)
     (t 'none)))
 
-(defun side-toggle--open-window (buffer)
-  "Blah BUFFER."
-  (display-buffer-in-side-window
-    buffer
-    '((side . right)
-      (window-parameters . ((side-toggle . t))))))
-
-(defun side-toggle--close-window (window)
-  "Blah WINDOW."
-  (delete-window window))
-
 (defun side-toggle--open-buffer ()
   "Blah."
   (get-buffer "*terminal*"))
 
+
 (defun side-toggle--find-window ()
   "Blah."
   (->> (window-list)
-       (-first (lambda (w) (window-parameter w 'side-toggle)))))
+       (-first (lambda (w) (window-parameter w 'side-toggle--window-tag)))))
 
 (defun side-toggle--is-buffer (buffer)
   "Blah BUFFER."
-  (s-starts-with? "*termi" (buffer-name buffer)))
-
-(side-toggle--find-buffer)
+  (and
+   (s-starts-with? "*termi" (buffer-name buffer))
+   (buffer-live-p buffer)))
 
 (defun side-toggle--find-buffer ()
   "Blah."
   (->> (buffer-list)
-       (-first (lambda (b)
-                 (and (side-toggle--is-buffer b) (buffer-live-p b))))))
+       (-first 'side-toggle--is-buffer)))
 
 (provide 'side-toggle)
 ;;; side-toggle.el ends here
