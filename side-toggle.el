d880f8a45a9d92c02c98f82e280f0fd8970e7c0c
Jason Holloway <miceeatnicerice@hotmail.com>
2020-11-08 00:13:27 +0000

    implicit spec

--- side-toggle.el
+++ side-toggle.el
@@ -9,55 +9,70 @@
 ;;; -*- lexical-binding t; -*-
 
 
-(-first 'side-toggle--is-buffer (buffer-list))
+;; (-first 'side-toggle--is-buffer (buffer-list))
 
 
-(side-toggle--find-window)
+;; (side-toggle--find-window)
 
-(window-total-width
- (side-toggle--find-window))
+;; (window-total-width
+;;  (side-toggle--find-window))
 
 
+;; (side-toggle--current-mode)
+
+;; (side-toggle--find-buffer)
+;; (side-toggle--open-buffer)
+
+
+
+;; (window-parameters
+;;  (side-toggle--find-window))
+
+
+(defvar side-toggle--specs)
+(setq side-toggle--specs
+      '((test . ((tag . test)
+                 (params . ((side . right))))
+              )))
+
+;; (side-toggle--set-spec 'test)
+;; (side-toggle--tag)
+
+
+(defvar side-toggle--spec)
+(setq side-toggle--spec '())
+
+
+
+(side-toggle-toggle 'test)
+
+side-toggle--spec
+
+(side-toggle--set-spec 'test)
 (side-toggle--current-mode)
 
-(side-toggle--find-buffer)
-(side-toggle--open-buffer)
-
-(side-toggle--find-window)
 (side-toggle--open-window (side-toggle--find-buffer))
-
-(window-parameters
- (side-toggle--find-window))
-
-(defun side-toggle--open-window (buffer)
-  "Blah BUFFER."
-  (display-buffer-in-side-window
-    buffer
-    '((side . right)
-      (window-parameters . ((side-toggle--window-tag . t))))))
-
-(defun side-toggle--close-window (window)
-  "Blah WINDOW."
-  (let ((width (window-total-width window)))
-    (set-frame-parameter nil 'side-toggle-width 123)
-    (delete-window window)))
-
-(frame-parameters nil)
-
-(side-toggle-toggle)
+(side-toggle--close-window (side-toggle--find-window))
 
 
-(defconst side-toggle--buffer-name-prefix " *side-toggle-")
-(defconst side-toggle--window-tag "side-toggle")
+(seq-map 'window-parameters (window-list))
+(side-toggle--find-window)
+
+(frame-parameter nil 'side-toggle-state)
+(set-frame-parameter nil 'side-toggle-state nil)
+
+
 
 ;;;###autoload
-(defun side-toggle-toggle ()
-  "Blah."
+(defun side-toggle-toggle (tag)
+  "Blah TAG."
   (interactive)
+  (side-toggle--set-spec tag)
   (pcase (side-toggle--current-mode)
     ('visible (side-toggle--close-window (side-toggle--find-window)))
     ('exists  (side-toggle--open-window (side-toggle--find-buffer)))
     ('none    (side-toggle--open-window (side-toggle--open-buffer)))))
+
 
 (defun side-toggle--current-mode ()
   "Blah."
@@ -66,15 +81,10 @@
     ((side-toggle--find-buffer) 'exists)
     (t 'none)))
 
+
 (defun side-toggle--open-buffer ()
   "Blah."
   (get-buffer "*terminal*"))
-
-
-(defun side-toggle--find-window ()
-  "Blah."
-  (->> (window-list)
-       (-first (lambda (w) (window-parameter w 'side-toggle--window-tag)))))
 
 (defun side-toggle--is-buffer (buffer)
   "Blah BUFFER."
@@ -86,6 +96,41 @@
   "Blah."
   (->> (buffer-list)
        (-first 'side-toggle--is-buffer)))
+
+
+(defun side-toggle--tag ()
+  "Blah."
+  (map-elt side-toggle--spec 'tag))
+
+(defun side-toggle--set-spec (tag)
+  "Blah TAG."
+  (setq side-toggle--spec (map-elt side-toggle--specs tag)))
+
+
+(defun side-toggle--find-window ()
+  "Blah."
+  (->> (window-list)
+        (-first (lambda (w) (window-parameter w (side-toggle--tag))))))
+
+(defun side-toggle--open-window (buffer)
+  "Blah BUFFER."
+  (let*
+      ((params (append
+                (map-elt side-toggle--spec 'params)
+                `((window-parameters . ((,(side-toggle--tag) . t))))))
+       (window (display-buffer-in-side-window buffer params))
+       (width1 (window-total-width window))
+       (state  (frame-parameter nil 'side-toggle-state))
+       (width2 (map-elt state (side-toggle--tag) 60))
+       (delta  (- width2 width1)))
+    (window-resize window delta t)))
+
+(defun side-toggle--close-window (window)
+  "Blah WINDOW."
+  (let ((width  (window-total-width window)))
+    (map-put (frame-parameter nil 'side-toggle-state) (side-toggle--tag) width)
+    (delete-window window)))
+
 
 (provide 'side-toggle)
 ;;; side-toggle.el ends here
